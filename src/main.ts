518b6dde5b9fafc80482ec810f577fe29a4f2611
Anderson Santo <12055277+santo-ti@users.noreply.github.com>
2023-09-24 01:17:21 -0300

    FEATURE: Add the main logger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const logger: Logger = new Logger('Main');
+
+  const app = await NestFactory.create(AppModule, { cors: true });
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
+
   await app.listen(3000);
+  logger.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
